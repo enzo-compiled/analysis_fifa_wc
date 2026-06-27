2f77c0c60e1385442fbb3fcb1df1de7fefe9bc3b
enzo <enzopaolo25@gmail.com>
2026-06-26 23:19:25 -0400

    Se crea archivop clean que limpia/agrega campos para ambos dataframes

---
+++ R/clean.R
@@ -0,0 +1,19 @@
+library(dplyr)
+library(tidyverse)
+
+worldcups <- read.csv("data/raw/worldcups.csv")
+wcmatches <- read.csv("data/raw/wcmatches.csv")
+
+head(worldcups)
+
+worldcups <- worldcups %>% mutate(
+  goals_per_game = goals_scored / games,
+  attendance_per_game = attendance / games
+)
+
+#wcmatches[wcmatches$outcome == "D", ]
+
+wcmatches <- wcmatches %>% mutate(
+  goals_per_match = home_score + away_score,
+  empate = outcome == "D"
+)

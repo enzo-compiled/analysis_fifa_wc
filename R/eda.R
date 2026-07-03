d77e832f923434555ed59ddaedac69dbe746f2a0
enzo <enzopaolo25@gmail.com>
2026-07-02 21:48:45 -0400

    se agrega plot de paises con más victorias en partidos

--- R/eda.R
+++ R/eda.R
@@ -14,3 +14,14 @@ p1 <- worldcups %>%
   labs(title = "Goles promedio por partido por Mundial",
        x = "Año", y = "Goles por partido")
 ggsave("output/plots/goals_per_game.png", p1)
+
+p2 <- wcmatches %>%
+  filter(!is.na(winning_team)) %>%
+  count(winning_team, sort = TRUE) %>%
+  slice_head(n = 10) %>%
+  ggplot(aes(x = reorder(winning_team, n), y = n, fill = winning_team)) +
+  geom_col(show.legend = FALSE) +
+  coord_flip() +
+  labs(title = "Top 10 paises con más victorias",
+       x = "Paises", y = "Victorias")
+p2

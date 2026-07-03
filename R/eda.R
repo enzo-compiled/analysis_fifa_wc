c6d0652cce5e564941f32f48e176cc416457b621
enzo <enzopaolo25@gmail.com>
2026-07-02 22:16:49 -0400

    Se agregan más plots

--- R/eda.R
+++ R/eda.R
@@ -43,7 +43,7 @@ p4 <- wcmatches %>%
   count(team1, team2, sort = TRUE) %>%
   slice_head(n = 5) %>%
   mutate(rivales = paste(team1, "vs", team2)) %>%
-  ggplot(aes(x = reorder(rivalry, n), y = n, fill = rivales)) +
+  ggplot(aes(x = reorder(rivales, n), y = n, fill = rivales)) +
   geom_col(show.legend = FALSE) +
   coord_flip() +
   geom_text(aes(label = n), hjust = -0.2) +
@@ -51,3 +51,23 @@ p4 <- wcmatches %>%
        x = "", y = "Partidos") + 
   theme(plot.title = element_text(hjust = 0))
 ggsave("output/plots/top_rivales.png", p4)
+
+p5 <- wcmatches %>%
+  ggplot(aes(x = goals_per_match)) +
+  geom_histogram(binwidth = 1, fill = "tomato", color = "white") +
+  facet_wrap(~ stage_type, ncol = 2) +
+  labs(title = "Distribución de goles por fase",
+       x = "Goles por partido", y = "Partidos") +
+  theme(plot.title = element_text(hjust = 0))
+ggsave("output/plots/goles_por_fase.png", p5)
+
+p6 <- worldcups %>%
+  ggplot(aes(x = attendance_per_game, y = goals_per_game)) +
+  geom_point(size = 3, alpha = 0.7) +
+  geom_smooth(method = "lm", color = "red", se = TRUE) +
+  geom_text(aes(label = year), vjust = -0.8, size = 3) +
+  labs(title = "Asistencia vs goles promedio por mundial",
+       x = "Asistencia promedio por partido",
+       y = "Goles promedio por partido") +
+  theme(plot.title = element_text(hjust = 0))
+ggsave("output/plots/asistencia_goles.png", p6)

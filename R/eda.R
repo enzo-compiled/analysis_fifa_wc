7c6ec40353e56d634c5d3ee18a8033d1431764c6
enzo <enzopaolo25@gmail.com>
2026-07-02 22:02:24 -0400

    Se agregan nuevos plots

--- R/eda.R
+++ R/eda.R
@@ -24,4 +24,30 @@ p2 <- wcmatches %>%
   coord_flip() +
   labs(title = "Top 10 paises con más victorias",
        x = "Paises", y = "Victorias")
-p2
+ggsave("output/plots/top_winners_games.png", p2)
+
+p3 <- worldcups %>%
+  count(winner, sort = TRUE) %>%
+  ggplot(aes(x = reorder(winner, n), y = n, fill = winner)) +
+  geom_col(show.legend = FALSE) +
+  coord_flip() +
+  labs(title = "Países con más mundiales",
+       x = "País", y = "Títulos")
+ggsave("output/plots/top_winners_final.png", p3)
+
+p4 <- wcmatches %>%
+  mutate(
+    team1 = if_else(home_team < away_team, home_team, away_team),
+    team2 = if_else(home_team < away_team, away_team, home_team)
+  ) %>%
+  count(team1, team2, sort = TRUE) %>%
+  slice_head(n = 5) %>%
+  mutate(rivales = paste(team1, "vs", team2)) %>%
+  ggplot(aes(x = reorder(rivalry, n), y = n, fill = rivales)) +
+  geom_col(show.legend = FALSE) +
+  coord_flip() +
+  geom_text(aes(label = n), hjust = -0.2) +
+  labs(title = "Top 5 rivalidades",
+       x = "", y = "Partidos") + 
+  theme(plot.title = element_text(hjust = 0))
+ggsave("output/plots/top_rivales.png", p4)
